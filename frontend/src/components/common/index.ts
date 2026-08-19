9bc79574cba794d1ff1a5e9875a6c91f3cc42d24
chahat mahtoliya <chahatmahtoliya@gmail.com>
2026-08-19 23:19:04 +0530

    Remove legacy frontend

--- frontend/src/components/common/index.ts
+++
@@ -1,3 +0,0 @@
-export { CouponModal } from './CouponModal';
-export { Pagination } from './Pagination';
-export { default as ScrollToTop } from './ScrollToTop';
--- frontend/src/components/features/index.ts
+++
@@ -1,6 +0,0 @@
-export { CouponCard } from './CouponCard';
-export { StoreCard } from './StoreCard';
-export { DealCard } from './DealCard';
-export { CategoryCard } from './CategoryCard';
-export { FilterSidebar } from './FilterSidebar';
-export { SeasonalBanner } from './SeasonalBanner';
